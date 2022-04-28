b7434a7ab08c0e0cf20f51b852db4cdfddc8052c
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2022-04-28 16:46:23 +0200

    Stock
    +StockRepository
    +DeliveryRepository
    +Product_stockRepository
    +SupplierRepository

---
+++ stock/src/main/java/be/technifutur/stock/repositories/StockRepository.java
@@ -0,0 +1,7 @@
+package be.technifutur.stock.repositories;
+
+import be.technifutur.stock.models.entities.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockRepository extends JpaRepository<Long, Stock> {
+}
