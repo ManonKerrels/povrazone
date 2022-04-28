4f046df92c41c3e52e0875e1367dffa711fc0c08
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2022-04-28 11:27:35 +0200

    Initial commit

---
+++ client/src/main/java/be/technifutur/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package be.technifutur.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
