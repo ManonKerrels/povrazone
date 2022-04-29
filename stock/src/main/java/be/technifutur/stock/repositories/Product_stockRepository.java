05d9f64f6c3ff6275879901cb50b9fef2b632500
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2022-04-29 09:38:47 +0200

    Stock
    +StockServiceImpl
    +Long

--- stock/src/main/java/be/technifutur/stock/repositories/Product_stockRepository.java
+++ stock/src/main/java/be/technifutur/stock/repositories/Product_stockRepository.java
@@ -3,5 +3,5 @@ package be.technifutur.stock.repositories;
 import be.technifutur.stock.models.entities.Product_stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Product_stockRepository extends JpaRepository<Long, Product_stock> {
+public interface Product_stockRepository extends JpaRepository<Product_stock, Long> {
 }
